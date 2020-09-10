fd0b0d4e4ede950241863373f9b00514ea9d4323
Shanghai Fierce <monitechi8@gmail.com>
2020-09-09 21:03:46 -0500

    js pseudocode for functions

---
+++ assets/js/ingredients.js
@@ -0,0 +1,25 @@
+// 1. create variables of major sections to use within all javascript
+
+//////// 2. get localstorage
+
+// 3. collect all input ingredients
+// // create variables from all input ingredients
+
+//////// 4. set localstorage
+
+// 5. create function for getting ingredients data from API
+function getIngredients(){
+    // fetch from spoonacular
+    fetch()
+    .then()
+
+    ////// 6. set localstorage
+}
+
+// 6. display recipe function
+    // for loop for ingredients
+
+// A 7. run second API for drinks to match with the recipe
+    // display on page
+
+// A 8. run the display function again based on button
