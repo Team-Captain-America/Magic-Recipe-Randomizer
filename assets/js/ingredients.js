f6340898f7a737797f7ae3aa5acca23e7c1c3279
Shanghai Fierce <monitechi8@gmail.com>
2020-09-12 02:03:26 -0500

    updated html, css and js function order

--- assets/js/ingredients.js
+++ assets/js/ingredients.js
@@ -1,39 +1,82 @@
-// (M) 1. create variables of major sections to use within all javascript
+// (A) 1. create variables of major sections to use within all javascript
 
-//// (M) 2. get localstorage
+// (A) 3. function to collect all input ingredients
 
-// (M) 3. collect all input ingredients
-// //  create variables from all input ingredients
-// var userInput = document.getElementById('ing0').val;
-// $("#submit").click(function(event) {
-//     $('#input').val("Ingredient 0");
-// })
+// create localstorage for upcoming array
+var list = JSON.parse(localStorage.getItem('ingredients')) || [];
 
-// $(document).ready(function () { 
-//     $(":text")
-//     // On button click, get value 
-//     // of input control Show alert 
-//     // message box 
-//     $("#submit").click(function () { 
-//         var inputE1 = $("#input0").val(); 
-//         alert(inputE1); 
-//     }); 
-// }); 
+function formInput() {
+    // $(document).ready(function() {
+    //  create input variables from all ingredients
+    $("#submittext").on("click", function(event){
+        var inputE1 = $('#input0').val();
+        var inputE2 = $('#input1').val();
+        var inputE3 = $('#input2').val();
+        var inputE4 = $('#input3').val();
+        var inputE5 = $('#input4').val();
 
-// //// (M) 4. set localstorage
-// localStorage.setItem('ingredients', JSON.stringify(userIngredients));
+        console.log(inputE1, inputE2, inputE3, inputE4, inputE5);
 
+    //     var taskArray = new Array();
+    //     $("input[name=input]").each(function() {
+    //    taskArray.push($(this).val());
 
-// cocktail function to get random suggestion based on previous click
+    //    console.log(taskArray);
+    //     });
+
+        // update ingredients list into array
+        list.push(ingredients);
+
+        displayIngredients(list);
+
+        //// (M) 4. set localstorage
+        localStorage.setItem('ingredients', JSON.stringify(list));
+
+        // (O) 5. create function for getting ingredients data from API with input variables
+        function getIngredients(id){
+
+            $.ajax({
+            url: "https://api.spoonacular.com/recipes/"+id+"/information?apiKey=54dabc814050472fb2b3631a332e7a58",
+            success:function(data) {
+                console.log('success', data);
+            }
+            });
+        };
+    });
+
+    // (O) 6. display recipe function
+    function displayIngredients(list) {
+        // Empties out the html
+        $('#ingredient-name').empty();
+
+        // Iterates over the 'list'
+        for (var i = 0; i < list.length; i++) {
+            // Sets the input values
+            var ingredientE1 = $('<p>');
+            ingredientE1.text(list[i]);
+            toDoClose.addClass('subtitle');
+
+            // <figure class="image is-128x128">
+            //   <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+            // </figure><br>
+            // <div class="content">
+            //   <p class="" id="ingredient-description"></p>
+        }
+    };
+}
+
+// (M) 7. run second API for drinks to match with the recipe
+// Cocktail function to get random suggestion based on previous click
 var getDrink = function() {
     var getDrinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
+    // Display the result onpage
     $.getJSON(getDrinkUrl).done(function(data){
         console.log(data);
         $.each(data.drinks, function(index, drink) {
             // getting the random drink name
             var drinkName = drink.strDrink;
-            var drinkTextE1 = $("<p>").html("Try this cocktail:<br><br>" + index + drinkName);
+            var drinkTextE1 = $("<p>").html(index + drinkName);
             drinkTextE1.appendTo("#drink");
        
             // getting the drink image thumb 
@@ -42,16 +85,31 @@ var getDrink = function() {
             var drinkTextE2 = $("<img>").html('img src="' + index + drinkImage +'">');
             console.log(drinkImage);
             drinkTextE2.appendTo("#drinkimage");
+
+            // getting the drink ingredients
+            var drinkIngredients = [drink.strIngredient];
+            console.log(drinkIngredients);
+            var drinkTextIng1 = $("<ul>").html(index + drinkIngredients);
+            drinkTextIng1.appendTo("#drinkingredient");
+
+            // for (var i = 0; i < drink.strIngredient[i].length; i++) {
+            //     // Sets the `list` item's value as text of this <p> element
+            //     var drinkTextIng1 = $("<ul>").html(index + drinkIngredients);
+            //     drinkTextIng1.text(list[i]);
+            //     drinkTextIng1.appendTo("#drinkingredient");
+                
+            // var array = [drink.strIngredient, i];
+            // console.log(array);
    
             // getting the recipe for the drink
             var drinkRecipe = drink.strInstructions;
             // probably just need to remove the "0" and it will work
-            var drinkTextE3 = $("<p>").html("How to make it:<br><br>" + index + drinkRecipe);
+            var drinkTextE3 = $("<p>").html(index + drinkRecipe);
             console.log(drinkRecipe);
             drinkTextE3.appendTo("#drinkrecipe");
         })
     });
-};
+}
 
 $("#drinkidea").on('click', getDrink);
 
@@ -61,38 +119,12 @@ var resetDrink = function() {
     // clear content in drink section
     $("#drink").html("");
     $("#drinkimage").html("");
-    $("#drinkingredients").html("");
+    $("#drinkingredient").html("");
     $("#drinkrecipe").html("");
 
     // get a new cocktail suggestion
     getDrink(); 
 }
 
-$("#resetdrink").on('click', resetDrink);
-
-// (O) 5. create function for getting ingredients data from API
-// function getIngredients(){
-//     // fetch from spoonacular
-//     fetch()
-//     .then()
-
-function getIngredients(id){
-
-    $.ajax({
-    url: "https://api.spoonacular.com/recipes/"+id+"/information?apiKey=54dabc814050472fb2b3631a332e7a58",
-    success:function(data) {
-        console.log('success', data);
-
-    }
-    });
-
-    //// 6. set localstorage
-
-
-// (O) 6. display recipe function
-    // for loop for ingredients
-
-// (A) 7. run second API for drinks to match with the recipe
-    // display on page
-
 // (A) 8. run the display function again based on button
+$("#resetdrink").on('click', resetDrink);
